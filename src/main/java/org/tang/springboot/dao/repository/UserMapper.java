cff47d551216b3eaed74092a277a28dfa588c5ad
330882908@qq.com <330882908@qq.com>
2018-10-08 22:35:05 +0800

    springboot

---
+++ src/main/java/org/tang/springboot/dao/repository/UserMapper.java
@@ -0,0 +1,7 @@
+package org.tang.springboot.dao.repository;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import org.tang.springboot.dao.entity.User;
+
+public interface UserMapper extends BaseMapper<User> {
+}
